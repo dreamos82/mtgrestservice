c5a34138c05bd68b9a0294e9e872debe54924a98
Ivan <ivan.gualandri@gmail.com>
2016-08-17 13:53:17 +0100

    Remove unused code.

--- mtgrestservice.go
+++ mtgrestservice.go
@@ -6,34 +6,6 @@ import (
 )
 
 func main() {
-  /*xml_file, err := os.Open("assets/magicsymbols.xml")
-  if( err != nil ){
-    fmt.Println("Error opening file: ", err)
-    return
-  }
-
-  defer xml_file.Close()
-
-  b, _ := ioutil.ReadAll(xml_file)
-
-  var q datamodel.Query
-	xml.Unmarshal(b, &q)
-
-  editionsMap = make(map[string]datamodel.Edition)
-
-	for _, edition := range q.Editions {
-    		//fmt.Printf("\t%s - %s - %s\n", edition.Code, edition.Names[0].Name, edition.Names[0].Lang)
-        EditionsMap[edition.Code] = edition
-        result, err := json.Marshal(edition)
-        if ( err != nil) {
-          fmt.Println("Error: ", err)
-          return
-        }
-        fmt.Printf("%s\n", result)
-	}
-  fmt.Println(EditionsMap["EMN"])*/
-  /*EditionsMap = datamodel.LoadMap()
-  fmt.Println(EditionsMap["EMN"])*/
   fmt.Println("Launching sever")
   server.InitServer()
 }
