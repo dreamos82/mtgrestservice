13075cd1ba7558ae929417cbed648da405af196c
Ivan Gualandri <ivan.gualandri@gmail.com>
2016-10-29 11:35:21 +0100

    Add marshal for CardsComposition. This should complete task #3

--- datamodel/xmlmarshalers.go
+++ datamodel/xmlmarshalers.go
@@ -34,3 +34,36 @@ func (name NameNode) MarshalXML(e *xml.Encoder, start xml.StartElement) (err err
 	err = e.EncodeElement(name.Name, start)
 	return err
 }
+
+// MarshalXML generate XML output for CardsComposition
+func (cards CardsComposition) MarshalXML(e *xml.Encoder, start xml.StartElement) (err error) {
+	if (CardsComposition{} == cards) {
+		return nil
+	}
+	err = e.EncodeToken(start)
+	if err != nil {
+		return err
+	}
+	if cards.Common > 0 {
+		err = e.EncodeElement(cards.Common, xml.StartElement{Name: xml.Name{Local: "common"}})
+	}
+	if cards.Lands > 0 {
+		err = e.EncodeElement(cards.Lands, xml.StartElement{Name: xml.Name{Local: "lands"}})
+	}
+	if cards.Mythic > 0 {
+		err = e.EncodeElement(cards.Mythic, xml.StartElement{Name: xml.Name{Local: "mythic"}})
+	}
+	if cards.Other > 0 {
+		err = e.EncodeElement(cards.Other, xml.StartElement{Name: xml.Name{Local: "other"}})
+	}
+	if cards.Oversize > 0 {
+		err = e.EncodeElement(cards.Oversize, xml.StartElement{Name: xml.Name{Local: "oversize"}})
+	}
+	if cards.Rare > 0 {
+		err = e.EncodeElement(cards.Rare, xml.StartElement{Name: xml.Name{Local: "rare"}})
+	}
+	if cards.Uncommon > 0 {
+		err = e.EncodeElement(cards.Uncommon, xml.StartElement{Name: xml.Name{Local: "uncommon"}})
+	}
+	return e.EncodeToken(xml.EndElement{Name: start.Name})
+}
