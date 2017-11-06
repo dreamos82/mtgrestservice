803d8e45c3f8f96bfacbe4f98c6f658a6e599750
Ivan Gualandri <ivan.gualandri@gmail.com>
2017-11-06 18:17:24 +0000

    Add config file parsing, return map with all value read from the file

--- config/config.go
+++ config/config.go
@@ -3,24 +3,54 @@ package config
 import (
   "os"
   "fmt"
+  "bufio"
+  "strings"
+  "errors"
 )
 
-//Handle application configuration
-type Configuration struct {
-  Port  string
-  MaxConnections  string
-  AssetsFolder  string
-  HostName  string
-}
-
-func ReadConfigFile(fileName string){
+func ReadConfigFile(fileName string) (map[string]string, error){
   configFile, err := os.Open(fileName)
-  fmt.Println("Config")
   if err != nil {
     fmt.Println(err)
-    return
-  }
-  if configFile != nil {
-    fmt.Println("Open")
+    return nil, err
+  } else if configFile != nil {
+    scanner := bufio.NewScanner(configFile)
+    //configuration := NewConfiguration()
+    var configurationmap map[string]string
+    configurationmap = make(map[string]string)
+    for scanner.Scan() {             // internally, it advances token based on sperator
+        property := strings.Split(scanner.Text(), "=")
+        SetConfigurationProperty(&configurationmap, property)
+    }
+    return configurationmap, nil
+  } else {
+    err = errors.New("emit macho dwarf: elf header corrupted")
+    return nil, err
   }
 }
+
+func SetConfigurationProperty(configuration *map[string]string, property []string){
+  if(len(property) == 2){
+    (*configuration)[property[0]] = property[1]
+  }
+}
+
+
+/*func SetConfigurationProperty(configuration *Configuration, property []string){
+
+  if(len(property) == 2){
+    if strings.Compare(property[0], "port") == 0 {
+      configuration.Port = property[1]
+    }
+    if strings.Compare(property[0], "hostname") == 0{
+      configuration.HostName = property[1]
+    }
+    if strings.Compare(property[0], "assets") == 0 {
+      configuration.AssetsFolder = property[1]
+    }
+    if strings.Compare(property[0], "maxconnections") == 0{
+      configuration.MaxConnections = property[1]
+    }
+
+  }
+}*/
--- mtgrestservice.go
+++ mtgrestservice.go
@@ -7,36 +7,12 @@ import (
 )
 
 func main() {
-	/*xml_file, err := os.Open("assets/magicsymbols.xml")
-	  if( err != nil ){
-	    fmt.Println("Error opening file: ", err)
-	    return
-	  }
-
-	  defer xml_file.Close()
-
-	  b, _ := ioutil.ReadAll(xml_file)
-
-	  var q datamodel.Query
-		xml.Unmarshal(b, &q)
-
-	  editionsMap = make(map[string]datamodel.Edition)
-
-		for _, edition := range q.Editions {
-	    		//fmt.Printf("\t%s - %s - %s\n", edition.Code, edition.Names[0].Name, edition.Names[0].Lang)
-	        EditionsMap[edition.Code] = edition
-	        result, err := json.Marshal(edition)
-	        if ( err != nil) {
-	          fmt.Println("Error: ", err)
-	          return
-	        }
-	        fmt.Printf("%s\n", result)
-		}
-	  fmt.Println(EditionsMap["EMN"])*/
-	/*EditionsMap = datamodel.LoadMap()
-	  fmt.Println(EditionsMap["EMN"])*/
 	fmt.Println("Reading configuration")
-	config.ReadConfigFile("test.conf")
-	fmt.Println("Launching server")
-	server.InitServer()
+	configuration, err := config.ReadConfigFile("config.properties")
+	fmt.Println(configuration)
+	fmt.Println(err)
+	if err == nil && configuration != nil {
+		fmt.Println("Launching server")
+		server.InitServer()
+	}
 }
